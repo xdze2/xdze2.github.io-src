088fe2d1e0f101fa9f160b437d16748f8899bffb
xdze2 <iopzae@gmail.com>
2017-05-02 12:10:56 +0200

    add static dir

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = ''
 
 PATH = 'content'
 STATIC_PATHS = ['physique/images', 'statistiques/images', 'static', 'images',
-                'about/images']
+                'about/images', 'optimisation/images']
 
 USE_FOLDER_AS_CATEGORY = False
 DISPLAY_PAGES_ON_MENU = False
